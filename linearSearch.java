8f1526f44e461af6f2a4f65226ac3c3fe7df458f
kishor <kishorgbss3@gmail.com>
2024-01-25 21:29:59 +0530

    add File

---
+++ linearSearch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/**
+ * linearSearch
+ */
+public class linearSearch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the size of Array");
+            int size = sc.nextInt();
+
+            int[] arr = new int[size];
+            System.out.println("Enter the no in the array");
+            for(int i=0; i<size; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Enter the to be Searched");
+            int search = sc.nextInt();
+
+            for(int no : arr){
+                if(no == search){
+                    System.out.println(search + " is present in the array");
+                    return;
+                }
+            } 
+            System.out.println(search + " is not present in the array");
+    }
+    
+}
